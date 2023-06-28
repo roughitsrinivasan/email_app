215326ea7869c125216e9f77afdf7cba31915d15
Sudhir <sudirsha02@gmail.com>
2023-06-28 08:22:24 +0530

    commit b4 rewamp

--- App.js
+++ App.js
@@ -1,79 +1,44 @@
-import React, { Component } from "react";
-import { Button, SafeAreaView, StyleSheet, Alert, Text } from "react-native";
-//Importing the installed libraries
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { MaterialIcons } from '@expo/vector-icons';
+import Button from './src/components/Button';
 import * as FS from "expo-file-system";
-import * as ImagePicker from "expo-image-picker";
-
-import Login from "./src/components/Login";
-
-export default class App extends Component {
-  //Class components have a render() function. Whatever is returned inside it is rendered as a React element ...
-  
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      cameraRollPer: null,
-      disableButton: false,
-      
-    };
-  }
-
-  async componentDidMount() {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    this.setState((state, props) => {
-      return {
-        cameraRollPer: status === "granted",
-        disableButton: false,
-      };
-    });
-  }
-
-  pickMedia = async () => {
-    this.setState((state, props) => {
-      return {
-        cameraRollPer: state.cameraRollPer,
-        disableButton: true,
-      };
-    });
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      base64: true,
-    });
-    if (result.cancelled) {
-      return;
-    }
-    if (result.type == "image") {
-      await this.toServer({
-        type: result.type,
-        base64: result.base64,
-        uri: result.uri,
-      });
-    } else {
-      let base64 = await this.uriToBase64(result.uri);
-      await this.toServer({
-        type: result.type,
-        base64: base64,
-        uri: result.uri,
-      });
-    }
-  };
 
 
-  toServer = async (mediaFile) => {
-    let type = mediaFile.type;
-    let schema = "http://";
-    let host = "192.168.1.4";
-    let route = "";
-    let port = "5000";
+
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, []);
+
+
+  const toServer= async (name,email) => {
+    let type = "image";
+    let schema = "";
+    let host = "http://3e2c-14-97-224-30.ngrok-free.app";
+    let route = `/image?name=${name}&email=${email}`;
+    let port = "";
     let url = "";
     let content_type = "";
-    type === "image"
-      ? ((route = "/image"), (content_type = "image/jpeg"))
-      : ((route = "/video"), (content_type = "video/mp4"));
+    // type === "image"
+    //   ? ((route = ""), (content_type = "image/jpeg"))
+    //   : ((route = "/video"), (content_type = "video/mp4"));
     url = schema + host + ":" + port + route;
 
-    let response = await FS.uploadAsync(url, mediaFile.uri, {
+    let response = await FS.uploadAsync(url,image, {
       headers: {
         "content-type": content_type,
       },
@@ -85,38 +50,131 @@ export default class App extends Component {
     console.log(response.body);
   };
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        {/* {this.state.cameraRollPer ? (
-          <Button
-            title="Pick From Gallery"
-            disabled={this.state.disableButton}
-            onPress={async () => {
-              await this.pickMedia();
-              this.setState((s, p) => {
-                return {
-                  cameraRollPer: s.cameraRollPer,
-                  disableButton: false,
-                };
-              });
-            }}
-          />
-        ) : (
-          <Text>Camera Roll Permission Required ! </Text>
-        )} */}
-        <Login />
-      </SafeAreaView>
-    );
-  }
-}
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
+  const savePicture = async () => {
+    if (image) {
+      try {
+        const asset = await MediaLibrary.createAssetAsync(image);
+        alert('Picture saved! 🎉');
+        toServer();
+        setImage(null);
+        console.log('saved successfully');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      {!image ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          ref={cameraRef}
+          flashMode={flash}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingHorizontal: 30,
+            }}
+          >
+            <Button
+              title=""
+              icon="retweet"
+              onPress={() => {
+                setType(
+                  type === CameraType.back ? CameraType.front : CameraType.back
+                );
+              }}
+            />
+            <Button
+              onPress={() =>
+                setFlash(
+                  flash === Camera.Constants.FlashMode.off
+                    ? Camera.Constants.FlashMode.on
+                    : Camera.Constants.FlashMode.off
+                )
+              }
+              icon="flash"
+              color={flash === Camera.Constants.FlashMode.off ? 'gray' : '#fff'}
+            />
+          </View>
+        </Camera>
+      ) : (
+        <Image source={{ uri: image }} style={styles.camera} />
+      )}
+
+      <View style={styles.controls}>
+        {image ? (
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingHorizontal: 50,
+            }}
+          >
+            <Button
+              title="Re-take"
+              onPress={() => setImage(null)}
+              icon="retweet"
+            />
+            <Button title="Save" onPress={savePicture} icon="check" />
+          </View>
+        ) : (
+          <Button title="Take a picture" onPress={takePicture} icon="camera" />
+        )}
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#000',
+    padding: 8,
+  },
+  controls: {
+    flex: 0.5,
+  },
+  button: {
+    height: 40,
+    borderRadius: 6,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#E9730F',
+    marginLeft: 10,
+  },
+  camera: {
+    flex: 5,
+    borderRadius: 20,
+  },
+  topControls: {
+    flex: 1,
   },
 });
